c6a5a17ed75f505b38dbb8387b4c0c976723a197
Ahi Bhushan Mahto <114595802+ahibhushan@users.noreply.github.com>
2022-11-07 21:31:05 +0530

    Update InfixToPostfixEvaluation.java

--- Day24/InfixToPostfixEvaluation.java
+++ Day24/InfixToPostfixEvaluation.java
@@ -109,8 +109,6 @@ public class InfixToPostfixEvaluation {
     String s1 = a.infixToPostfix("2+3-1");
     System.out.println(a.postfixToEvaluation(s1));
     
-    String s2 = a.infixToPostfix("2+3*4");
-    System.out.println(a.postfixToEvaluation(s2));
     
     String s3 = a.infixToPostfix("3*(4+5)-6/(1+2)");
     System.out.println(a.postfixToEvaluation(s3));
