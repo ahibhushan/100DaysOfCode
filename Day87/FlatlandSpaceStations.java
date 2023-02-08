30d658fe8f0d6c1dc5c611d200bfc8c3acf72899
Ahi Bhushan Mahto <ahibhushan7@gmail.com>
2023-02-08 15:38:21 +0530

    Day87

---
+++ Day87/FlatlandSpaceStations.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class FlatlandSpaceStations {
+
+    // Complete the flatlandSpaceStations function below.
+    static int flatlandSpaceStations(int n, int[] c) {
+        LinkedList<Integer> list=new LinkedList<>();
+        if(n==c.length)
+        return 0;
+        else
+        {
+            for(int i=0;i<n;i++)
+            {
+              int min=Math.abs(c[0]-i);
+            for(int j=0;j<c.length;j++)  
+            {   
+            int res=Math.abs(c[j]-i);
+            if(res<min)
+            min=res;
+            }
+        list.add(min);
+            }
+        return Collections.max(list);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[] c = new int[m];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = flatlandSpaceStations(n, c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
